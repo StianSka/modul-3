a05bdca2deef7203e759d96464e0f609f769c64f
StianSka <116058912+StianSka@users.noreply.github.com>
2023-03-22 15:30:30 +0100

    la til pp kopi

---
+++ uke 2/ppUke2/ppUke2/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ppUke2
+{
+    internal class Program
+    {
+        //Oppgave - Social Media
+
+        //Lag applikasjon FriendFace.
+        //Det er tiltenkt at dette skal bli det nyeste kule sosiale mediet som tar verden med storm.
+        //Her trenger en bruker en profilside med diverse info om seg(ta gjerne inspirasjon fra andre sosiale medier),
+
+        //Du må lage en metode som heter “AddFriend” og en metode “RemoveFriend”.
+
+        //Når programmet starter opp skal du lage en hovedbruker som er “innlogget”.
+        //Du må også opprette flere forskjellige brukere som lever på det sosiale mediet.
+
+        //    Lag et kommandbasert meny i konsollen der du kan:
+
+        //Legge til forskjellige brukere som venn(på den som er innlogget)
+        //Fjerne brukere som venn
+        //Printe ut en liste av alle man har lagt til som venn
+        //Velge en av vennene og printe ut profilinformasjonen deres.
+        static void Main(string[] args)
+        {
+
+            User[] users = new[]
+            {
+                new User("Kurt", new List<string>(){"Bosted: Viken", "Favorittfarge: Grilla", "Kjeledyr: Nei, men gryte" }),
+                new User("Kåre", new List<string>(){"Bosted: Harstad", "Favorittfarge: Blågul", "Kjeledyr: Kattestrofe i C-dur" }),
+                new User("Willy", new List<string>(){"Bosted: Svartedal", "Favorittfarge: Rainbow, bitches", "Kjeledyr: Stein" }),
+                new User("Eva", new List<string>(){"Bosted: Rævland", "Favorittfarge: Lysebeige", "Kjeledyr: Vrengt polvått" }),
+                new User("Kristin", new List<string>(){"Bosted: Transylvania", "Favorittfarge: Blodrødt", "Kjeledyr: Flaggermus" }),
+
+            };
+            Console.WriteLine("Hvem er du?");
+            var userName = Console.ReadLine();
+            var mainUser = new User(userName, new List<string>());
+            Console.WriteLine("Hallo " + userName);
+            WaitingForUser(users, mainUser);
+        }
+
+        private static void WaitingForUser(User[] users, User mainUser)
+        {
+            Console.WriteLine("Skriv inn 1 for å se andre brukere eller 2 bare venner 3 for å legge til venner eller 4 for å fjerne venner eller 5 for å velge noen å se informasjonen til");
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "1":
+                    ListUsers(users);
+                    break;
+                case "2":
+                    mainUser.ListMyFriends();
+                    break;
+                case "3":
+                    mainUser.AddFriend(users, mainUser);
+                    break;
+                case "4":
+                    mainUser.RemoveFriend(mainUser);
+                    break;
+                case "5":
+                    mainUser.ShowFriendProfile(users, mainUser);
+                    break;
+                default:
+                    Console.WriteLine("Ugyldig command, prøv igjen.");
+                    break;
+            }
+            WaitingForUser(users, mainUser);
+        }
+
+
+        static void ListUsers(User[] users)
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
+        }
+    }
+}
---
+++ uke 2/ppUke2/ppUke2/User.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppUke2
+{
+    internal class User
+    {
+        public string Name { get; private set; }
+        private List<string> _list = new List<string>();
+
+        public User(string name, List<string> list)
+        {
+            Name = name;
+            _list = list;
+        }
+
+        public void ListMyFriends()
+        {
+            if (_list.Count != 0)
+            {
+                int temp = this._list.Count;
+                for (int i = 0; i < temp; i++)
+                {
+                    Console.WriteLine(this._list[i]);
+                }
+            }
+            else Console.WriteLine("Du har ingen venner");
+        }
+
+        public void ShowFriendProfile(User[] users, User mainUser)
+        {
+            if (_list.Count != 0)
+            {
+                ListMyFriends();
+                Console.WriteLine("skriv navnet til personen du ønsker å se profilen til");
+                string friendProfile = Console.ReadLine();
+                foreach (var item in mainUser._list)
+                {
+                    if (item == friendProfile)
+                    {
+                        WriteProfileItems(item, users);
+                        return;
+                    }
+                }
+
+                Console.WriteLine("Fant ikke venn");
+            }
+        }
+
+        private void WriteProfileItems(string item, User[] users)
+        {
+            foreach (var user in users)
+            {
+                if (user.Name == item)
+                {
+                    foreach (var stuff in user._list)
+                    {
+                        Console.WriteLine(stuff);
+                    }
+                }
+            }
+        }
+
+        public void AddFriend(User[] users, User mainUser)
+        {
+            Console.WriteLine("Skriv navnet til din nye venn:");
+            var friendToAdd = Console.ReadLine();
+            foreach (var user in users)
+            {
+                if (user.Name.Contains(friendToAdd))
+                {
+                    mainUser._list.Add(user.Name);
+                    Console.WriteLine($"Hurra, {user.Name} er nå din nye venn!");
+                    return;
+                }
+            }
+            Console.WriteLine("Fant ikke navnet, prøv igjen.");
+        }
+
+        public void RemoveFriend(User mainUser)
+        {
+            if (_list.Count == 0)
+            {
+                Console.WriteLine("Her var det tomt.");
+                return;
+            }
+
+            Console.WriteLine("Skriv navnet til den personen du vil fjerne:");
+            var friendToRemove = Console.ReadLine();
+            foreach (var user in mainUser._list)
+            {
+                if (user.Contains(friendToRemove))
+                {
+                    mainUser._list.Remove(user);
+                    Console.WriteLine($"Hade bra, {friendToRemove}.");
+                    return;
+                }
+
+
+            }
+            Console.WriteLine($"Fant ikke {friendToRemove}.");
+
+        }
+
+    }
+
+}
