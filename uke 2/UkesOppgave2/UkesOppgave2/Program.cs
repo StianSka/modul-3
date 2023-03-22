04de873053eaf750a33edd699ee8737cd1ed33c0
StianSka <116058912+StianSka@users.noreply.github.com>
2023-03-22 12:32:52 +0100

    pensum sjekk oppgave uke 2

---
+++ uke 2/UkesOppgave2/UkesOppgave2/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace UkesOppgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students;
+            students = new Student[6];
+            CreateStudents(students);
+            ShowMenu(students);
+        }
+        static void ShowMenu(Student[] students)
+        {
+            Console.WriteLine("Velg et kurs for å se deltakere");
+            Console.WriteLine("1: Matte");
+            Console.WriteLine("2: Norsk");
+            Console.WriteLine("3: Samfunsfag");
+            Console.WriteLine("4: Engelsk");
+            string selectedCoure = Console.ReadLine();
+            string choise = CheckChoise(selectedCoure);
+            ListParticapents(choise, students);
+        }
+        static string CheckChoise(string choise)
+        {
+            string course = "nope";
+            switch (choise)
+            {
+                case "1":
+                    course = ("Matte");
+                    break;
+                case "2":
+                    course = ("Norsk");
+                    break;
+                case "3":
+                    course = ("Samfunsfag");
+                    break;
+                case "4":
+                    course = ("Engelsk");
+                    break;
+                default:
+                    course = ("nope");
+                    break;
+            }
+            return course;
+        }
+        static void ListParticapents(string choise, Student[] students)
+        {
+            if(choise == "nope") { return; }
+            for (int i = 0; i < students.Length; i++)
+            {
+               if ( students[i].IsParticapent(choise) == true)
+                {
+                    students[i].ShowAll();
+                }
+            }
+            ShowMenu(students);
+        }
+        static void CreateStudents(Student[] students) {
+            string[] names = { "Per", "Petter", "Alex", "Mia", "Gunnar", "Trude" };
+            int[] ages = { 21, 24, 30, 25, 17, 29 };
+            string[] courses = {"Matte","Norsk","Samfunsfag", "Engelsk" };
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                int rng = RngNums(courses);
+                students[i] = new Student(names[i], ages[i], courses[rng]);
+            }
+        }
+        static int RngNums(string[] temp)
+        {
+            Random random = new Random();
+            int rng = random.Next(0,temp.Length);
+            return rng;
+        }
+    }
+}
---
+++ uke 2/UkesOppgave2/UkesOppgave2/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkesOppgave2
+{
+    internal class Student
+    {
+        private string _name;
+        private int _age;
+        private string _course;
+    
+        public Student(string name, int age, string course) {
+            _name = name;
+            _age = age;
+            _course = course;
+        }
+        public void ShowName() {
+            Console.WriteLine(this._name);
+        }
+        public void ShowAge() {
+            Console.WriteLine(this._age);
+        }
+        public void ShowCourse() {
+            Console.WriteLine(this._course);
+        }
+        public void ShowAll() {
+            Console.WriteLine(this._name);
+            Console.WriteLine(this._age);
+            Console.WriteLine(this._course);
+        }
+        public bool IsParticapent(string course) {
+            if (course == this._course) { return true; }
+            else { return false; }
+        }
+
+
+    }
+}
