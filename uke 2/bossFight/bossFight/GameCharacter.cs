79802fe06d67f40819145daef62e4f897b8b5a7a
StianSka <116058912+StianSka@users.noreply.github.com>
2023-03-23 15:24:03 +0100

    boss fight

---
+++ uke 2/bossFight/bossFight/GameCharacter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bossFight
+{
+    internal class GameCharacter
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int Strength { get; private set; }
+        public int Stamina { get; private set; }
+
+        public GameCharacter(string name, int health, int strength, int stamina) 
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+        }
+        public void Fight(GameCharacter reciver)
+        {
+            if (this.Stamina <= 0)
+            {
+                this.Recharge();
+                return;
+            }
+            if (this.Name== "Boss") { 
+                this.Strength = Program.RngNum(0,31);
+            }
+
+            reciver.Health = reciver.Health - this.Strength;
+            this.Stamina = this.Stamina - 10;
+            Console.WriteLine(this.Name + " hitt " + reciver.Name + " for " + this.Strength + " " + reciver.Name +
+                " health is now " + reciver.Health);
+        }
+        
+        public void Recharge()
+        {
+            if (this.Name == "Boss")
+            {
+                Console.WriteLine(this.Name + " is recharging");
+                this.Stamina = 10;
+            }
+            else if (this.Name == "Hero")
+            {
+                Console.WriteLine(this.Name + " is recharging");
+                this.Stamina = 40;
+            }
+        }
+    }
+}
---
+++ uke 2/bossFight/bossFight/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace bossFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rngStrength = RngNum(0, 31);
+            GameCharacter hero = new GameCharacter("Hero", 100, 20, 40);
+            GameCharacter boss = new GameCharacter("Boss", 400, rngStrength , 10);
+            StartFight(hero, boss);
+        }
+
+        static void StartFight(GameCharacter hero, GameCharacter boss)
+        {
+            bool hasWon = false;
+            string winner = "";
+            while (!hasWon)
+            {
+                hero.Fight(boss);
+                    if (boss.Health <= 0) { hasWon = true; winner = hero.Name; }
+                boss.Fight(hero);
+                    if (hero.Health <= 0) { hasWon = true; winner = boss.Name; }
+
+            }
+            Console.WriteLine("and the winner is " + winner);
+        }
+
+        public static int RngNum(int min, int max)
+        {
+            Random random = new Random();
+            int rng = random.Next(min, max);
+            return rng;
+        }
+    }
+}
