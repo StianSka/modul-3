9173ba57d12e2a7c2316b7e2d4cd9bb488eebd5e
StianSka <116058912+StianSka@users.noreply.github.com>
2023-03-24 14:57:53 +0100

    rpg game

---
+++ uke 2/rpgGame/rpgGame/Goblin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpgGame
+{
+    internal class Goblin
+    {
+        private string _name;
+        private int _maxHp;
+        private int _hp;
+        private int _strength;
+        private int _potionCount;
+        private int _speed;
+
+        public Goblin(string name, int maxHp, int hp, int strength, int potionCount, int speed)
+        {
+            _name = name;
+            _maxHp = maxHp;
+            _hp = hp;
+            _strength = strength;
+            _potionCount = potionCount;
+            _speed = speed;
+        }
+
+        public void DrinkPotion() {
+            if (this._potionCount <= 0) { return; }
+            if (this._hp + 20 > this._maxHp) { this._hp = this._maxHp; this._potionCount -= 1; }
+            else { this._hp += 20; this._potionCount -= 1; }
+
+        }
+        public int BasicAttack()
+        {
+            return this._strength;
+        }
+    }
+}
---
+++ uke 2/rpgGame/rpgGame/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace rpgGame
+{
+    internal class Program
+    {
+        static void Main() {
+            MainMenu();
+        }
+        static void MainMenu() {
+            Console.Clear();
+            Console.WriteLine("Wlecome to RPG Game");
+            Console.WriteLine("Press the number of the option you would like to chose.");
+            Console.WriteLine("1: Start new game");
+            Console.WriteLine("2: Load save");
+            Console.WriteLine("3: Exit");
+            string choise = Console.ReadLine();
+            DetermedChoise(choise );
+        }
+
+        static void DetermedChoise(string choise) {
+            switch (choise)
+            {
+                case "1":
+                    StartGame();
+                    break;
+                case "2":
+                    Console.WriteLine("not implemented: press any key to return to main menu");
+                    Console.ReadKey();
+                    Main();
+                    break; 
+                case "3":
+                    Console.WriteLine("Good bye");
+                    break;
+                default:
+                    Console.WriteLine("Unexpected command: press any key to return to main menu");
+                    Console.ReadKey();
+                    Main();
+                    break;
+            }
+        }
+
+        static void StartGame() {
+            Console.Clear();
+            Console.WriteLine("Please type in a name for your first hero");
+            string name = Console.ReadLine();
+            CheckName(name);
+            int hp = 100;
+            int strength = 20;
+            int potionCount = 3;
+            int speed = 30;
+            var hero1 = new Goblin( name, hp, hp,  strength,  potionCount, speed);
+            name = "Peasant 1";
+            hp = 80;
+            potionCount = 2;
+            speed = 20;
+            var enemy1 = new Goblin(name, hp, hp, strength, potionCount, speed);
+            StartBattle(hero1, enemy1);
+        }
+
+        static void StartBattle(object hero, object enemy) {
+            
+        }
+
+        static void CheckName(string name) {
+            if (name == null || name == "")
+            {
+                Console.WriteLine("Invalid name: press any key to try again ");
+                Console.ReadKey();
+                StartGame();
+            }
+            else { return; }
+        }
+    }
+}
