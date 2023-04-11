af26ad8c97dab1236de81febec098a3b0dd91e84
StianSka <116058912+StianSka@users.noreply.github.com>
2023-04-11 15:32:42 +0200

    stuff

---
+++ uke 4/readWriteToFile/readWriteToFile/Creature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readWriteToFile
+{
+    internal class Creature
+    {
+        public string Name { get;}
+        public string Species { get; }
+        public int Age { get; }
+        public string Gender { get; }
+        public Creature(string name, string species, int age, string gender)
+        {
+            Name = name;
+            Species = species;
+            Age = age;
+            Gender = gender;
+        }
+
+
+    }
+}
---
+++ uke 4/readWriteToFile/readWriteToFile/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace readWriteToFile
+{
+    internal class Program
+    {
+        static string path = "C:\\Users\\stian\\Documents\\GitHub\\modul-3-stuff\\uke 4\\readWriteToFile\\test.txt";
+        static void Main(string[] args)
+        {
+            MakeCreature();
+        }
+        static void MakeCreature()
+        {
+            string[] names = {"Kåre", "Pål", "Trude", "Mia"};
+            string[] species = {"Elefant", "Spekkhogger", "Kenguru" };
+            string name = names[RandomNum(0,names.Length)];
+            string thispecies = species[RandomNum(0, species.Length)];
+            int age  = RandomNum(0, 15);
+            int result = RandomNum(0, 2);
+            string gender = result == 1 ? "han" : "hun";
+            Creature creature = new Creature(name, thispecies, age, gender);
+            //ReadCreature();
+            //ReadSpecificLine(4);
+            //EditFile(path, "test");
+            PrintCreatureDeets(creature);
+        }
+        static void PrintCreatureDeets(Creature creatue)
+        {
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine(creatue.Name);
+            writer.WriteLine(creatue.Species);
+            writer.WriteLine(creatue.Age);
+            writer.WriteLine(creatue.Gender);
+            writer.WriteLine();
+            writer.Close();
+        }
+        //static void EditFile(string filePath, string newContent)
+        //{
+        //    using (StreamWriter writer = new StreamWriter(filePath, false))
+        //    {
+        //        writer.Write(newContent);
+        //        writer.Close();
+        //    }
+        //}
+        static void ReadCreature()
+        {
+            StreamReader reader = new StreamReader(path);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+        }
+        static void ReadSpecificLine(int lineNumber)
+        {
+            StreamReader reader = new StreamReader(path);
+            string line;
+            int currentLine = 1;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (currentLine == lineNumber)
+                {
+                    Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                    break;
+                }
+                currentLine++;
+            }
+            reader.Close();
+        }
+        static int RandomNum(int min, int max)
+        {
+            Random random = new Random();
+            int rng = random.Next(min, max);
+            return rng;
+        }
+    }
+}
